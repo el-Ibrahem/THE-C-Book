e9add7490437c93c399195db3fa00df6947535d6
el-Ibrahem <abrahymalajmdy2@gmail.com>
2026-07-21 22:53:19 +0300

    sv1

--- first-steps.c
+++ first-steps.c
@@ -1,7 +1,17 @@
 #include <stdio.h> /*these are headers that include contents and utilities and more...*/
 #include <stdlib.h>
-
+/*
 int main(){
     puts("Hello, World!");
     return EXIT_SUCCESS;
 }
+*/
+
+int main(){
+    if(puts("Hello, World!") == EOF){
+    return EXIT_FAILURE;
+    // code here never excutes  
+    }
+    return EXIT_SUCCESS;   
+}
+// this revise if the puts func returns an E0F
---
+++ printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h> /*these are headers that include contents and utilities and more...*/
+#include <stdlib.h>
+/*
+    int main(){
+        printf("%s\n" , "Hellow world!");
+        return EXIT_SUCCESS;
+    }
+*/
+
+int main(){
+    if (printf("%s\n" , "Hellow world!") < 0)
+    {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
